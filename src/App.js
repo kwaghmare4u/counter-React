09e38e8f868d90bfe37d5c0377bde1226208f992
kwaghmare4u <kwaghmare4u@gmail.com>
2023-10-01 19:41:20 +0530

    React Increment Decrement Counter application

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {useState} from 'react';
+
+
+const App = () => {
+
+  const[counter , setCounter] = useState(0);
+
+ 
+  return (
+    <div className='App'>
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>+</button>
+    </div>
+  );
+
+}
+
+export default App;
